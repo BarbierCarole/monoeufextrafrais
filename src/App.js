3f819496f46d118e30f3e29b2cb0edfa529832ae
BarbierCarole <carole83460@gmail.com>
2021-05-28 11:13:58 +0200

    test react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useForm} from 'react-hook-form';
 
 function App() {
+  const {register, handleSubmit} = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello from V2</h1>
+        </header>
+        
+      </div>
+      <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="dlc">Date limite de consommation</label>
+        <input type="text" id="dlc" nome="dlc" ref={register} />
+      </form>        
+      </div>
     </div>
   );
 }
